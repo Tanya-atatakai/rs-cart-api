74a843ff482f9ca67bdb79c7f35f7ec2a72a53aa
Tatiana <tatiana_priemova_epam.com>
2023-11-20 19:09:41 +0400

    Task 8 (Integration with SQL Database)

--- src/cart/services/cart.service.ts
+++ src/cart/services/cart.service.ts
@@ -1,31 +1,94 @@
 import { Injectable } from '@nestjs/common';
 
-import { v4 } from 'uuid';
-
 import { Cart } from '../models';
+import { config, getPgClient } from 'src/db';
+import { Client } from 'pg';
 
 @Injectable()
 export class CartService {
-  private userCarts: Record<string, Cart> = {};
+  async findByUserId(userId: string): Promise<Cart> {
+    const pgClient = getPgClient();
 
-  findByUserId(userId: string): Cart {
-    return this.userCarts[ userId ];
+    try {
+      const pgClient = new Client(config);
+      await pgClient.connect();
+
+      const query = `
+      SELECT 
+        carts.id AS cart_id,
+        carts.user_id,
+        carts.created_at,
+        carts.updated_at,
+        carts.status,
+        cart_items.product_id,
+        cart_items.count
+      FROM 
+        carts
+      LEFT JOIN 
+        cart_items ON carts.id = cart_items.cart_id
+      WHERE 
+        carts.user_id = '${userId}';
+    `;
+
+      const { rows } = await pgClient.query(query);
+
+      if (rows.length === 0) {
+        return null; // No cart found for the given user ID
+      }
+
+      const cart: Cart = {
+        id: rows[0].cart_id,
+        items: rows.map((row) => ({
+          product: {
+            id: row.product_id,
+            title: row.title,
+            description: row.description,
+            price: row.price,
+          },
+          count: row.count,
+        })),
+      };
+
+      return cart;
+    } catch (e) {
+      console.log('Error in CartService, findByUserId:', e);
+    } finally {
+      await pgClient.end();
+    }
   }
 
-  createByUserId(userId: string) {
-    const id = v4(v4());
-    const userCart = {
-      id,
-      items: [],
-    };
+  async createByUserId(userId: string): Promise<Cart | null> {
+    const pgClient = getPgClient();
 
-    this.userCarts[ userId ] = userCart;
+    try {
+      await pgClient.connect();
 
-    return userCart;
+      const insertCartQuery = `
+        INSERT INTO carts (user_id, created_at, updated_at, status)
+        VALUES
+            ('${userId}', NOW(), NOW(), 'open')
+        RETURNING id
+      `;
+
+      const cartResult = await pgClient.query(insertCartQuery);
+      const newCart = cartResult.rows[0];
+
+      const createdCart: Cart = {
+        id: newCart.id,
+        items: [],
+      };
+
+      return createdCart;
+    } catch (e) {
+      console.log('Error in CartService, createByUserId:', e);
+      return null;
+    } finally {
+      await pgClient.end();
+    }
   }
 
-  findOrCreateByUserId(userId: string): Cart {
-    const userCart = this.findByUserId(userId);
+  async findOrCreateByUserId(userId: string): Promise<Cart> {
+    const userCart = await this.findByUserId(userId);
 
     if (userCart) {
       return userCart;
@@ -34,22 +97,82 @@ export class CartService {
     return this.createByUserId(userId);
   }
 
-  updateByUserId(userId: string, { items }: Cart): Cart {
-    const { id, ...rest } = this.findOrCreateByUserId(userId);
+  async updateByUserId(userId: string, { items }: Cart): Promise<Cart | null> {
+    const pgClient = getPgClient();
 
-    const updatedCart = {
-      id,
-      ...rest,
-      items: [ ...items ],
+    try {
+      await pgClient.connect();
+      // Find or create cart
+      const existingOrNewCart = await this.findOrCreateByUserId(userId);
+
+      // Delete old cart items
+      const deleteItemsQuery = `
+        DELETE FROM cart_items
+        WHERE cart_id = '${existingOrNewCart.id}';
+      `;
+      await pgClient.query(deleteItemsQuery);
+
+      // Insert new cart items
+      const insertItemsQuery = items
+        .map(
+          (item) => `
+            INSERT INTO cart_items (cart_id, product_id, count)
+            VALUES ('${existingOrNewCart.id}', '${item.product.id}', ${item.count});
+          `,
+        )
+        .join('');
+
+      await pgClient.query(insertItemsQuery);
+
+      // Update cart date
+      const updateCartQuery = `
+        UPDATE carts
+        SET updated_at = NOW()
+        WHERE id = '${existingOrNewCart.id}'
+        AND status = 'open';
+      `;
+
+      await pgClient.query(updateCartQuery);
+
+      const updatedCart: Cart = {
+        ...existingOrNewCart,
+        items: [...items],
+      };
+
+      return updatedCart;
+    } catch (e) {
+      console.log('Error in CartService, updateByUserId:', e);
+      return null;
+    } finally {
+      await pgClient.end();
     }
-
-    this.userCarts[ userId ] = { ...updatedCart };
-
-    return { ...updatedCart };
   }
 
-  removeByUserId(userId): void {
-    this.userCarts[ userId ] = null;
-  }
+  async removeByUserId(userId: string): Promise<void> {
+    const pgClient = getPgClient();
 
+    try {
+      await pgClient.connect();
+
+      const existingCart = await this.findByUserId(userId);
+
+      if (existingCart) {
+        // Update cart date
+        const updateCartQuery = `
+        UPDATE carts
+        SET status = 'ordered',
+        updated_at = NOW()
+        WHERE id = '${existingCart.id}'
+      `;
+
+        await pgClient.query(updateCartQuery);
+      } else {
+        throw new Error('Cart not found');
+      }
+    } catch (e) {
+      console.log('Error in CartService, removeByUserId:', e);
+    } finally {
+      await pgClient.end();
+    }
+  }
 }
